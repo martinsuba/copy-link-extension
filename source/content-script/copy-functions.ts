6727629595986a3a84c4710546a18b69acac287d
Martin Suba <martin.suba01@gmail.com>
2019-12-20 14:02:11 +0100

    Use copy-text-to-clipboard

--- source/content-script/copy-functions.ts
+++ source/content-script/copy-functions.ts
@@ -1,20 +1,27 @@
-import { saveTextToClipboard } from './clipboard';
+import copy from 'copy-text-to-clipboard';
 import { addToHistory } from '../utils/storage';
+
+function saveTextToClipboard(text: string): void {
+  const success = copy(text);
+  if (!success) {
+    throw new Error('Failed to copy.');
+  }
+}
 
 export async function copyWebsiteURL(): Promise<void> {
   const currentURL = window.location.href;
-  await saveTextToClipboard(currentURL);
+  saveTextToClipboard(currentURL);
   await addToHistory(currentURL);
 }
 
 export async function copyImageURL(image: HTMLImageElement): Promise<void> {
   const { src } = image;
-  await saveTextToClipboard(src);
+  saveTextToClipboard(src);
   await addToHistory(src);
 }
 
 export async function copyLinkURL(link: HTMLAnchorElement): Promise<void> {
   const { href } = link;
-  await saveTextToClipboard(href);
+  saveTextToClipboard(href);
   await addToHistory(href);
 }
